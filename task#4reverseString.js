1e7902a541d6583f2ac919f910d874f718794ca9
Samvel Antonyan <samvelantonyan1998@gmail.com>
2024-08-27 13:34:02 +0400

    add task #4 function reverseString

---
+++ task#4reverseString.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString("Hello World"));
+console.log(reverseString("JavaScript"));
+console.log(reverseString("OpenAI"));
