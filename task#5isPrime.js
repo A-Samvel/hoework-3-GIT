9244f5e17370ea13edea21ac116e9f86f56003c0
Samvel Antonyan <samvelantonyan1998@gmail.com>
2024-08-27 13:48:54 +0400

    add task #5 function isPrime

---
+++ task#5isPrime.js
@@ -0,0 +1,12 @@
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(7));
+console.log(isPrime(10));
+console.log(isPrime(13));
